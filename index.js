5e01301fa33c0175b5653197f9924a958def7dbe
tsmx <dev@tsmx.net>
2024-07-26 21:20:00 +0200

    Updated AWS Docker base image to NodeJS 20

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export const handler = async (event, context) => {
     console.log('EVENT: \n' + JSON.stringify(event, null, 2));
-    return { info: 'Hello from AWS Lambda!' };
+    return { info: 'Hello from AWS Lambda with NodeJS 20!' };
 };
